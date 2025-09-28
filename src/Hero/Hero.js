834e5e34965753fa49b074e4642d9006e0b27625
Ryan Chen <ryanchenwry@gmail.com>
2025-09-28 13:19:57 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero">
+        <h1>Personal Budget</h1>
+        <h2>A personal-budget management app</h2>
+        <h3>By: Ryan Chen</h3>
+    </div>
+  );
+}
+
+export default Hero;
